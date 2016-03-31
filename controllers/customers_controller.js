3612be930ebbbedfded4bb826cdf3bb70393f717
Jovanny Cruz <jovannypcg@yahoo.com>
2016-03-31 22:41:05 +0000

    Se agrega funcionalidad para obtener a todos los clientes y borrarlos

--- controllers/customers_controller.js
+++ controllers/customers_controller.js
@@ -56,8 +56,33 @@ exports.createCustomer = function(request, response, next) {
 }
 
 exports.getCustomers = function(request, response, next) {
-    response.send(200, {OK: 'getCustomers'});
-    return next();
+    const logger = request.log;
+
+    /*
+     * El parentesis vacio dentro de find() indica que se traiga a todos los
+     * clientes, una vez que termina la consulta, se entra a la seccion 'then'
+     * donde se puede acceder a los clientes encontrados, los llame
+     * 'foundCustomers'
+     */
+    Customer.find({}).exec().then(foundCustomers => {
+        let responseObject = {
+            status: 200,
+            customers: foundCustomers
+        }
+
+        response.send(200, responseObject);
+        return next();
+    }).catch(error => {
+        logger.error(`get customers: ${error}`);
+        response.send(500, {
+            error: {
+                status: 500,
+                detail: error
+            }
+        });
+
+        return next();
+    });
 }
 
 exports.getCustomer = function(request, response, next) {
@@ -66,6 +91,66 @@ exports.getCustomer = function(request, response, next) {
 }
 
 exports.deleteCustomer = function(request, response, next) {
-    response.send(200, {OK: 'deleteCustomer'});
-    return next();
+    const logger = request.log;
+
+    let customerQuery = {
+        _id: request.params.customerId
+    };
+
+    Customer.findOne(customerQuery).exec().then(customer => {
+        /*
+         * Este if se ejecuta si la busqueda del cliente no arroja resultados,
+         * es decir, que el cliente no existe, entonces devolvemos un 404.
+         */
+        if (!customer) {
+            logger.error(`delete customer: customer not found`);
+            response.send(404, {
+                status: 404,
+                error: {
+                    detail: 'Customer not found'
+                }
+            });
+
+            /*
+             * Esta linea nos manda a la clausula catch, para que no se
+             * siga ejecutando lo demas.
+             */
+            throw new Error();
+        }
+
+        /*
+         * Si no se ejecuta el if, significa que el cliente si existe y podemos
+         * eliminarlo. Esta linea regresa una promesa (un hilo) que se
+         * atrapara en el siguiente 'then'
+         */
+        return customer.remove();
+    }).then(deletedCustomer => {
+        /*
+         * Al entrar aqui, significa que la linea 'return customer.remove()' fue
+         * satisfactoria. La instruccion 'customer.remove' regresa al cliente
+         * que fue eliminado, y en este 'then' lo llamamos 'deletedCustomer'.
+         *
+         * Generalmente, cuando un elemento es eliminado, se devuelve junto
+         * con la respuesta, por eso lo incluyo en el responseObject. Nota:
+         * el atributo 'customer' de 'responseObject' envia al cliente que se
+         * elimino de la BD. En este punto el
+         */
+        let responseObject = {
+            status: 200,
+            customer: deletedCustomer
+        };
+
+        response.send(200, responseObject);
+        return next();
+    }).catch(error => {
+        logger.error(`delete customer: ${error}`);
+        response.send(500, {
+            status: 500,
+            error: {
+                detail: error
+            }
+        });
+
+        return next();
+    });
 }
